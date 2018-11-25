524e0b7ede2247575c42a2358d2dee4611ee6414
asabogal <asabogal.git@gmail.com>
2018-11-25 17:27:47 -0500

    add leagues teams route

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get '/leagues/:id/matches/next', to: 'leagues#next_matches', as: 'league_next_matches'
   get '/leagues/:id/matches/scheduled', to: 'leagues#scheduled_matches', as: 'league_scheduled_matches'
   get '/leagues/:id/matches/all', to: 'leagues#all_matches', as: 'league_all_matches'
+  get 'leagues/:id/teams', to: 'leagues#teams', as: 'league_teams'
 
 end
