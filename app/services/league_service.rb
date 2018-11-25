01470b4d7a83f62619fa174e7c0327eeb930f5cf
asabogal <asabogal.git@gmail.com>
2018-11-25 17:00:28 -0500

    fix identation

--- app/services/league_service.rb
+++ app/services/league_service.rb
@@ -1,47 +1,49 @@
 class LeagueService
 
-	def self.get_league(id)
-			url = "https://api.football-data.org/v2/competitions/#{id}"
-			resp = Faraday.get url do |req|
-				req.headers['X-Auth-Token'] = ENV['AUTH_TOKEN']
-			end
-			
-			body = JSON.parse(resp.body)
-
-			if resp.success?
-				@response = body
-				else
-				@response = body["meta"]["errorDetail"]
-				end
-		
-		@response
+def self.find_league(id)
+	url = "https://api.football-data.org/v2/competitions/#{id}"
+	resp = Faraday.get url do |req|
+    req.headers['X-Auth-Token'] = ENV['AUTH_TOKEN']
 	end
-	
-	def self.get_matches(league, query="")
-		url = "https://api.football-data.org/v2/competitions/#{league}/matches?#{query}"
-		resp = Faraday.get url do |req|
+
+	body = JSON.parse(resp.body)
+
+	if resp.success?
+	@response = body
+	else
+	@response = body["meta"]["errorDetail"]
+	end
+
+	@response
+end
+
+def self.get_matches(league, query="")
+	url = "https://api.football-data.org/v2/competitions/#{league}/matches?#{query}"
+	resp = Faraday.get url do |req|
 		req.headers['X-Auth-Token'] = ENV['AUTH_TOKEN']
-		end
-	
-		body = JSON.parse(resp.body)
-	
-				if resp.success?
-				@response = body["matches"]
-				else
-				@response = body["meta"]["errorDetail"]
-				end
-		
-		@response
 	end
 
-	def self.get_matchday(league)
-    url = "https://api.football-data.org/v2/competitions/#{league}"
-    resp = Faraday.get url do |req|
-      req.headers['X-Auth-Token'] = ENV['AUTH_TOKEN']
-    end
-    body = JSON.parse(resp.body)
-    @matchday = body["currentSeason"]["currentMatchday"]
-    @matchday
-  end
+	body = JSON.parse(resp.body)
+
+	if resp.success?
+	@response = body["matches"]
+	else
+	@response = body["meta"]["errorDetail"]
+	end
+
+	@response
+end
+
+def self.get_matchday(league)
+	url = "https://api.football-data.org/v2/competitions/#{league}"
+	resp = Faraday.get url do |req|
+		req.headers['X-Auth-Token'] = ENV['AUTH_TOKEN']
+	end
+
+	body = JSON.parse(resp.body)
+
+	@matchday = body["currentSeason"]["currentMatchday"]
+	@matchday
+	end
 
 end
