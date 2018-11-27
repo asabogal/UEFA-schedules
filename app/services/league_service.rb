4f688f9964a93e513ad385568b904027e3e94544
asabogal <asabogal.git@gmail.com>
2018-11-26 20:58:12 -0500

    fix identation

--- app/services/league_service.rb
+++ app/services/league_service.rb
@@ -1,62 +1,62 @@
 class LeagueService
 
-def self.find_league(id)
-	url = "https://api.football-data.org/v2/competitions/#{id}"
-	resp = Faraday.get url do |req|
-    req.headers['X-Auth-Token'] = ENV['AUTH_TOKEN']
-	end
-
-	body = JSON.parse(resp.body)
-
-	if resp.success?
-	@response = body
-	else
-	@response = body["meta"]["errorDetail"]
-	end
-
-	@response
-end
-
-def self.get_matches(league, query="")
-	url = "https://api.football-data.org/v2/competitions/#{league}/matches?#{query}"
-	resp = Faraday.get url do |req|
-		req.headers['X-Auth-Token'] = ENV['AUTH_TOKEN']
-	end
-
-	body = JSON.parse(resp.body)
-
-	if resp.success?
-	@response = body["matches"]
-	else
-	@response = body["meta"]["errorDetail"]
-	end
-
-	@response
-end
-
-def self.get_matchday(league)
-	url = "https://api.football-data.org/v2/competitions/#{league}"
-	resp = Faraday.get url do |req|
-		req.headers['X-Auth-Token'] = ENV['AUTH_TOKEN']
-	end
-
-	body = JSON.parse(resp.body)
-
-	@matchday = body["currentSeason"]["currentMatchday"]
-	@matchday
-	end
-
-	def self.get_teams(league)
-		url = "https://api.football-data.org/v2/competitions/#{league}/teams"
-		resp = Faraday.get url do |req|
+  def self.find_league(id)
+    url = "https://api.football-data.org/v2/competitions/#{id}"
+    resp = Faraday.get url do |req|
       req.headers['X-Auth-Token'] = ENV['AUTH_TOKEN']
     end
 
-      body = JSON.parse(resp.body)
+    body = JSON.parse(resp.body)
 
-      @teams = body["teams"]
-      @teams
-		
-	end
+    if resp.success?
+    @response = body
+    else
+    @response = body["meta"]["errorDetail"]
+    end
+
+    @response
+  end
+
+  def self.get_matches(league, query="")
+    url = "https://api.football-data.org/v2/competitions/#{league}/matches?#{query}"
+    resp = Faraday.get url do |req|
+      req.headers['X-Auth-Token'] = ENV['AUTH_TOKEN']
+    end
+
+    body = JSON.parse(resp.body)
+
+    if resp.success?
+    @response = body["matches"]
+    else
+    @response = body["meta"]["errorDetail"]
+    end
+
+    @response
+  end
+
+  def self.get_matchday(league)
+    url = "https://api.football-data.org/v2/competitions/#{league}"
+    resp = Faraday.get url do |req|
+      req.headers['X-Auth-Token'] = ENV['AUTH_TOKEN']
+    end
+
+    body = JSON.parse(resp.body)
+
+    @matchday = body["currentSeason"]["currentMatchday"]
+    @matchday
+    end
+
+    def self.get_teams(league)
+      url = "https://api.football-data.org/v2/competitions/#{league}/teams"
+      resp = Faraday.get url do |req|
+        req.headers['X-Auth-Token'] = ENV['AUTH_TOKEN']
+      end
+
+        body = JSON.parse(resp.body)
+
+        @teams = body["teams"]
+        @teams
+      
+    end
 
 end
