5c0a6f1ecb01093bd721a67964106200d4427aa5
asabogal <asabogal.git@gmail.com>
2018-11-26 19:29:15 -0500

    add teams controller and show action

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,14 @@
+class TeamsController < ApplicationController
+
+  def show
+    binding.pry
+    @team = TeamService.find_team(params[:id])
+    json = @team.to_json
+
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: json }
+    end
+  end
+
+end
