3f40c3790e9735bfadd5c529f836654c7bd32669
asabogal <asabogal.git@gmail.com>
2018-11-26 19:30:25 -0500

    add team service

---
+++ app/services/team_service.rb
@@ -0,0 +1,20 @@
+class TeamService
+
+  def self.find_team(id)
+    url = "https://api.football-data.org/v2/teams/#{id}"
+    resp = Faraday.get url do |req|
+      req.headers['X-Auth-Token'] = ENV['AUTH_TOKEN']
+    end
+
+    body = JSON.parse[resp.body]
+
+    if resp.success?
+      @response = body
+    else
+      @response = body["meta"]["errorDetails"]
+    end
+
+    @response
+  end
+
+end
