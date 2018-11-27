8cda8ee956594abeeed2d6aea5d9ca1b8b0f8864
asabogal <asabogal.git@gmail.com>
2018-11-26 21:16:24 -0500

    add #get_matches to incorporate in leagues#show

--- app/services/team_service.rb
+++ app/services/team_service.rb
@@ -17,4 +17,21 @@ class TeamService
     @response
   end
 
+  def self.get_matches(id)
+    url = "https://api.football-data.org/v2/teams/#{id}/matches?status=SCHEDULED"
+    resp = Faraday.get url do |req|
+      req.headers['X-Auth-Token'] = ENV['AUTH_TOKEN']
+    end
+
+    body = JSON.parse(resp.body)
+
+    if resp.success?
+      @response = body["matches"]
+    else
+      @response = body["meta"]["errorDetails"]
+    end
+
+    @response
+  end
+
 end
