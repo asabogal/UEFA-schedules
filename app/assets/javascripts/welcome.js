31ccf84a93f3184858ab6a0c42fdfcc9f2a73de0
asabogal <asabogal.git@gmail.com>
2018-11-28 12:28:53 -0500

    initial leagues index view

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,32 @@
+
+$(function(){
+  showMatches()
+
+})
+
+
+// function showMatches(){
+//   $('.btn.btn-primary.league-button').on('click', function(e){
+//     e.preventDefault()
+//       $('.next-matches').load(`/leagues/${this.dataset.id}/matches/next .matches-table`)
+//     });
+
+//     //write this with $.ajax !!
+// };
+
+
+// function showMatches(){
+//   $('.btn.btn-primary.league-button').on('click', function(e){
+//     e.preventDefault()
+//       $.ajax({
+//         method: `GET`,
+//         url: `http://localhost:3000/leagues/${this.dataset.id}/matches/next.json`,
+//       }).done(function(resp){
+//         console.log(resp)
+//       })
+//     });
+// };
+
+
+
+      
