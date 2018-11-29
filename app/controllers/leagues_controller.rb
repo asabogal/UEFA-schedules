d29849285496c5ead83951f2a8141bb268f0eb65
asabogal <asabogal.git@gmail.com>
2018-11-28 21:29:10 -0500

    add initial pagination

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -26,17 +26,47 @@ class LeaguesController < ApplicationController
     end
   end
 
-  def next_matches
-    league = LeagueService.find_league(params[:id])
-    matchday = league["currentSeason"]["currentMatchday"] 
-    query = "matchday=#{matchday}"
-    @league_name = league["name"]
-    @matches = LeagueService.get_matches(params[:id], query)
-    
+  def current_matches
+    @league = LeagueService.find_league(params[:id])
+    @matchday = @league["currentSeason"]["currentMatchday"] 
+    @query = "matchday=#{@matchday}"
+    @league_name = @league["name"]
+    @matches = LeagueService.get_matches(params[:id], @query)
+   
     json = @matches.to_json
 
     respond_to do |format|
-      format.html { render :next_matches}
+      format.html { render :current_matches}
+      format.json { render json: json }
+      end
+  end
+
+  def previous_matches
+    @league = LeagueService.find_league(params[:id])
+    @matchday = @league["currentSeason"]["currentMatchday"] - 1
+    @query = "matchday=#{@matchday}"
+    @league_name = @league["name"]
+    @matches = LeagueService.get_matches(params[:id], @query)
+   
+    json = @matches.to_json
+
+    respond_to do |format|
+      format.html { render :current_matches}
+      format.json { render json: json }
+      end
+  end
+
+  def next_matches
+    @league = LeagueService.find_league(params[:id])
+    @matchday = @league["currentSeason"]["currentMatchday"] + 1
+    @query = "matchday=#{@matchday}"
+    @league_name = @league["name"]
+    @matches = LeagueService.get_matches(params[:id], @query)
+   
+    json = @matches.to_json
+
+    respond_to do |format|
+      format.html { render :current_matches}
       format.json { render json: json }
       end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root 'welcome#home'
 
   resources :leagues, only: [:index, :show]
-  get '/leagues/:id/matches/next', to: 'leagues#next_matches', as: 'league_next_matches'
+  get '/leagues/:id/matches/current', to: 'leagues#current_matches'
+  get '/leagues/:id/matches/next', to: 'leagues#next_matches'
+  get '/leagues/:id/matches/previous', to: 'leagues#previous_matches'
   get '/leagues/:id/matches/scheduled', to: 'leagues#scheduled_matches', as: 'league_scheduled_matches'
   get '/leagues/:id/matches/all', to: 'leagues#all_matches', as: 'league_all_matches'
   get 'leagues/:id/teams', to: 'leagues#teams', as: 'league_teams'
