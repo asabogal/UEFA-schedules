6e65092c853df97bf10f7c503fc5605db0f121be
asabogal <asabogal.git@gmail.com>
2018-11-29 20:59:15 -0500

    set model validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,9 @@
 class Review < ApplicationRecord
+
+  validates :rating, presence: true
+  validates :rating, length: { maximum: 5 }
+  validates :comment, presence: true
+  validates :comment, length: { maximum: 150 }
+  validates :reviewer, presence: true
+
 end
