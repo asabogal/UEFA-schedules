13ece14e83127646b50129fcd5ec712b96d8df4f
asabogal <asabogal.git@gmail.com>
2018-11-29 21:22:26 -0500

    add flash messages + finalize rails create action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,9 +7,11 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
       if @review.save
-        
+        flash[:success] = "Review successfuly created!"
+        redirect_to "/reviews"
       else
-
+        flash.now[:message] = "<strong>Please try again. There were some errors:</strong><br>".html_safe + @review.errors.full_messages.join("<br/>").html_safe
+        render :index
       end
   end
 
