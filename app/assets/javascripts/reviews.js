9742fc05bd12359a74ce1c08534cde7624278e48
asabogal <asabogal.git@gmail.com>
2018-11-30 17:12:39 -0500

    initial revievs ajax

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -6,16 +6,33 @@ getReviews()
 function getReviews(){
   $("#new_review").on('submit', function(e){
     e.preventDefault()
-
-    $.ajax({
-      type: "POST",
-      url: this.action,
-      data: $(this).serialize(),
-      success: function(response){
-        console.log(response)
-      }
-    });
-   
+    let values = $(this).serialize()
+    let posting = $.post('/reviews', values)
+    posting.done(function(data){
+      $('#review-list').prepend(renderData(data))
+    })
   });
 
 };
+
+function renderData(data){
+  console.log(data)
+
+  let html = `<div class="review-container bg-light">
+  <h1> Rating: ${data.rating} </h1>
+  <p> Comment: ${data.comment} </p>
+  <h5> Reviewer: ${data.reviewer} </h4>
+  </div>`
+  return html
+};
+
+
+
+// $.ajax({
+//   type: "POST",
+//   url: this.action,
+//   data: $(this).serialize(),
+//   success: function(response){
+//     console.log(response)
+//   }
+// });
