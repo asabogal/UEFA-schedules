ba4cf3d15431d49b62bf8c12f83d0a76a72df745
asabogal <asabogal.git@gmail.com>
2018-11-29 21:00:41 -0500

    setup initial create action and validation

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,6 +5,19 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    @review = Review.new(review_params)
+      if @review.save
+        
+      else
+
+      end
+  end
+
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :comment, :reviewer)
   end
 
 end
