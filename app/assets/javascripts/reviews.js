b0676c329906b9ec2fb57b53756e5211d5926e73
asabogal <asabogal.git@gmail.com>
2018-11-30 19:40:19 -0500

    refactor script and create class functionality

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -3,28 +3,61 @@ $(function(){
 getReviews()
 })
 
+class Review {
+  constructor (data){
+    this.rating = data.rating
+    this.comment = data.comment
+    this.reviewer = data.reviewer
+  }
+}
+
+Review.prototype.renderHTML = function(){
+  return `<div class="review-container bg-light">
+  <div class="star-rating" data-score=${this.rating}></div><br>
+  <p> ${this.comment} </p>
+    <em> Reviewer: ${this.reviewer} </em>
+  </div>
+  <br></br`
+};
+
 function getReviews(){
   $("#new_review").on('submit', function(e){
     e.preventDefault()
     let values = $(this).serialize()
     let posting = $.post('/reviews', values)
     posting.done(function(data){
-      $('#review-list').prepend(renderData(data))
+
+      const newReview = new Review(data)
+      const reviewHTML = newReview.renderHTML()
+      $('#review-list').prepend(reviewHTML)
+      setStars()
     })
   });
 
 };
 
-function renderData(data){
-  console.log(data)
+// function renderData(data){
 
-  let html = `<div class="review-container bg-light">
-  <h1> Rating: ${data.rating} </h1>
-  <p> Comment: ${data.comment} </p>
-  <h5> Reviewer: ${data.reviewer} </h4>
-  </div>`
-  return html
-};
+//   let html = `<div class="review-container bg-light">
+//   <div class="star-rating" data-score=${data.rating}></div><br>
+//   <p> ${data.comment} </p>
+//     <em> Reviewer: ${data.reviewer} </em>
+//   </div>
+//   <br></br`
+//   return html
+// };
+
+function setStars(){
+  $('.star-rating').raty({
+    path: '/assets/',
+    readOnly: true,
+    score: function() {
+        return $(this).attr('data-score');
+    }
+    });
+  };  
+
+ 
 
 
 
