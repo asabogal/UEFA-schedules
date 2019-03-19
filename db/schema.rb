4011510aef682a8e0217955be88aeef7f5caf437
asabogal <asabogal.git@gmail.com>
2019-03-19 18:18:48 -0400

    change db to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_11_29_235900) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "reviews", force: :cascade do |t|
     t.integer "rating"
     t.text "comment"
